79fc11454b6a0902740da72cf729bfe2f411a446
9ine <ergo9ine@gmail.com>
2018-11-05 17:07:51 +0900

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,74 +1,45 @@
-var frame=document.querySelector("frame").contentDocument;
-var Now=Number(document.querySelector("frame").contentDocument.querySelector("select[name=month]>[selected='']").value)
-var All=0,HighA=0,MiddleA=0,LowA=0,HighV=0,MiddleV=0,LowV=0,Kvar=0,all=0,kvar=0;
-var WinterH=[11,12,18,19,20,23];
-var WinterM=[10,13,14,15,16,17,21,22];
-var WFSL=[1,2,3,4,5,6,7,8,9,24];
-var FSH=[11,12,14,15,16,17];
-var FSM=[10,13,18,19,20,21,22,23];
-function Loop(i){
-	var k=i;
-	if(k>=13){k=k-12}
-	var vst1=Number(frame.querySelector(`.hori_table1:nth-child(4)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child(${k+2})>td:nth-child(2)`).innerHTML);
-	var vst2=Number(frame.querySelector(`.hori_table2:nth-child(5)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child(${k+2})>td:nth-child(2)`).innerHTML);
-	var kvar1=Number(frame.querySelector(`.hori_table1:nth-child(4)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child(${k+2})>td:nth-child(4)`).innerHTML);
-	var kvar2=Number(frame.querySelector(`.hori_table1:nth-child(4)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child(${k+2})>td:nth-child(5)`).innerHTML);
-	var kvar3=Number(frame.querySelector(`.hori_table2:nth-child(5)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child(${k+2})>td:nth-child(4)`).innerHTML);
-	var kvar4=Number(frame.querySelector(`.hori_table2:nth-child(5)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child(${k+2})>td:nth-child(5)`).innerHTML);
-	if(i<=12){All+=vst1,Kvar+=kvar1+kvar2,kvar+=kvar1+kvar2,all+=vst1}
-	else if(i>=13){All+=vst2,Kvar+=kvar3+kvar4,kvar+=kvar3+kvar4,all+=vst2}
+var b=document.querySelector("frame").contentDocument,
+	c=Number(document.querySelector("frame").contentDocument.querySelector("select[name=month]>[selected='']").value),
+	e=0,g=0,h=0,k=0,l=[11,12,18,19,20,23],m=[10,13,14,15,16,17,21,22],n=[1,2,3,4,5,6,7,8,9,24],u=[11,12,14,15,16,17],v=[10,13,18,19,20,21,22,23];
+function w(f){
+	var a=f;13<=a&&(a-=12);
+	var d=Number(b.querySelector(".hori_table1:nth-child(4)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child("+(a+2)+")>td:nth-child(2)").innerHTML),
+		p=Number(b.querySelector(".hori_table2:nth-child(5)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child("+(a+2)+")>td:nth-child(2)").innerHTML),
+		q=Number(b.querySelector(".hori_table1:nth-child(4)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child("+(a+2)+")>td:nth-child(4)").innerHTML),
+		r=Number(b.querySelector(".hori_table1:nth-child(4)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child("+(a+2)+")>td:nth-child(5)").innerHTML),
+		t=Number(b.querySelector(".hori_table2:nth-child(5)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child("+(a+2)+")>td:nth-child(4)").innerHTML);
+	a=Number(b.querySelector(".hori_table2:nth-child(5)>table:nth-child(1)>tbody:nth-child(1)>tr:nth-child("+(a+2)+")>td:nth-child(5)").innerHTML);
+	12>=f?(e+=d,g+=q+r,k+=q+r,h+=d):13<=f&&(e+=p,g+=t+a,k+=t+a,h+=p)
 }
-function Month(NowMonth){
-	function Low(M){
-		if(M>=11||M<=2){
-			kvar=0,all=0;
-			for(var i of WFSL){Loop(i)}
-			console.log(`겨울철 경부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}else if(M==3||M==4||M==5||M==9||M==10){
-			kvar=0,all=0;
-			for(var i of WFSL){Loop(i)}
-			console.log(`봄·가을철 경부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}else if(M==0||M>=13){console.log("Month Error.")}
-		else{
-			kvar=0,all=0;
-			for(var i of WFSL){Loop(i)}
-			console.log(`여름철 경부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}
-	};
-	function Middle(M){
-		if(M>=11||M<=2){
-			kvar=0,all=0;
-			for(var i of WinterM){Loop(i)}
-			console.log(`겨울철 중간부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}else if(M==3||M==4||M==5||M==9||M==10){
-			kvar=0,all=0;
-			for(var i of FSM){Loop(i)}
-			console.log(`봄·가을철 중간부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}else if(M==0||M>=13){console.log("Month Error.")}
-		else{
-			kvar=0,all=0;
-			for(var i of FSM){Loop(i)}
-			console.log(`여름철 중간부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}
-	};
-	function High(M){
-		if(M>=11||M<=2){
-			kvar=0,all=0;
-			for(var i of WinterH){Loop(i)}
-			console.log(`겨울철 최대부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}else if(M==3||M==4||M==5||M==9||M==10){
-			kvar=0,all=0;
-			for(var i of FSH){Loop(i)}
-			console.log(`봄·가을철 최대부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}else if(M==0||M>=13){console.log("Month Error.")}
-		else{
-			kvar=0,all=0;
-			for(var i of FSH){Loop(i)}
-			console.log(`여름철 최대부하 유효전력 ${all}, 무효전력 ${kvar}`)
-		}
-	};
-	Low(NowMonth),Middle(NowMonth),High(NowMonth);
-};
 console.clear();
-Month(Now);
-console.log(`총유효 전력 ${All.toString().slice(0,7)}, 총무효 전력 ${Kvar.toString().slice(0,6)}`)
+function consolelog(a,Load,h,k){
+	var M,L;
+	if(11<=a||2>=a){M=`겨울철`}else if(3==a||4==a||5==a||9==a||10==a){M=`봄·가을철`}else{M=`여름철`}
+	if(Load==1){L=`경부하`}else if(Load==2){L=`중간부하`}else{L=`최대부하`}
+	console.log(`${M} ${L} 유효전력 ${h}, 무효전력 ${k}`)
+}
+(function(f){(function(a){
+	if(11<=a||2>=a){h=k=0;for(var d in n)w(d);
+	consolelog(a,1,h,k)}
+	else if(3==a||4==a||5==a||9==a||10==a){h=k=0;for(d of n)w(d);
+	consolelog(a,1,h,k)}
+	else if(0==a||13<=a)console.log("Month Error.");else{h=k=0;for(d of n)w(d);
+	consolelog(a,1,h,k)}
+})(f);
+(function(a){
+	if(11<=a||2>=a){h=k=0;for(var d in m)w(d);
+	consolelog(a,2,h,k)}
+	else if(3==a||4==a||5==a||9==a||10==a){h=k=0;for(d of v)w(d);
+	consolelog(a,2,h,k)}
+	else if(0==a||13<=a)console.log("Month Error.");else{h=k=0;for(d of v)w(d);
+	consolelog(a,2,h,k)}
+})(f);
+(function(a){
+	if(11<=a||2>=a){h=k=0;for(var d in l)w(d);
+	consolelog(a,3,h,k)}
+	else if(3==a||4==a||5==a||9==a||10==a){h=k=0;for(d of u)w(d);
+	consolelog(a,3,h,k)}
+	else if(0==a||13<=a)console.log("Month Error.");else{h=k=0;for(d of u)w(d);
+	consolelog(a,3,h,k)}
+})(f)})(c);
+console.log(`총 유효전력 ${e.toString().slice(0,7)}, 총 무효전력 ${g.toString().slice(0,6)}`)
